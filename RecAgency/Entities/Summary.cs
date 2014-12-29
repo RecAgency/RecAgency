fc1796768d5cf4fcc71bc6d9fdea6fd9baa0abb4
Alexey-Yashunkin <zhukalexey1995@yandex.ru>
2014-12-30 00:13:07 +0300

    Добавил круд для аппликанта с представлениями

--- RecAgency/Entities/Summary.cs
+++ RecAgency/Entities/Summary.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace RecAgency.Entities
 {
     public class Summary
     {
+        [HiddenInput (DisplayValue = false)]
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public string Speciality { get; set; }
