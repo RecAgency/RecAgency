b20dfcd5610a33da3c61c61e7e27decb5fd68b21
vsh95 <v_sh95@mail.ru>
2014-12-30 00:53:53 +0300

    Start working on main page

--- RecAgency/Controllers/HomeController.cs
+++ RecAgency/Controllers/HomeController.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RecAgency.Abstract;
+using RecAgency.Concrete;
+using RecAgency.Entities;
 
 namespace RecAgency.Controllers
 {
     public class HomeController : Controller
     {
+              //  private IVacancyRepository Vrepository;
+              //  private ISummaryRepository Srepository;
+        public HomeController()//IVacancyRepository vrepo, ISummaryRepository srepo)
+        {
+           // this.Vrepository = vrepo;
+           // this.Srepository = srepo;
+        }
+
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
+            ViewBag.Message = "RecAgency";
             return View();
+        }
+
+
+        public ActionResult ListResume()
+        {
+            return View();//Srepository.Summaries.ToList());
+        }
+
+        public ActionResult ListVacancy()
+        {
+            return View();//Vrepository.Vacancies.ToList());
         }
 
         public ActionResult About()
