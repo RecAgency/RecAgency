1e09e1a2962cc6765a36fb9c172751e34d7685a4
vsh95 <v_sh95@mail.ru>
2014-12-29 19:30:31 +0300

    EmployerController. Vacancy.

--- RecAgency/Controllers/EmployerController.cs
+++ RecAgency/Controllers/EmployerController.cs
@@ -3,17 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RecAgency.Abstract;
+using RecAgency.Concrete;
+using RecAgency.Entities;
 
 namespace RecAgency.Controllers
 {
     public class EmployerController : Controller
     {
+        private IVacancyRepository repository;
+        public EmployerController(IVacancyRepository repo)
+        {
+            this.repository = repo;
+        }
+
         //
         // GET: /Employer/
 
         public ActionResult Index()
         {
-            return View();
+            return View(repository.Vacancies.Where(s => s.UserId == WebMatrix.WebData.WebSecurity.CurrentUserId));
+        }
+
+        public ActionResult Edit(int vacancyId)
+        {
+            Vacancy vacancy = repository.Vacancies.FirstOrDefault(s => s.Id == vacancyId);
+            return View(vacancy);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Vacancy vacancy)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveVacancy(vacancy, WebMatrix.WebData.WebSecurity.CurrentUserId);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(vacancy);
+            }
+        }
+
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Vacancy());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int vacancyId)
+        {
+            Vacancy deleteVacancy = repository.DeleteVacancy(vacancyId);
+            return RedirectToAction("Index");
         }
 
     }
